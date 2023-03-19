dd5214793b533aa16780edc3957617a11b4b3a9c
Bryan Biedenkapp <gatekeep@gmail.com>
2023-03-19 13:32:13 -0400

    update copyright header;

--- network/RESTAPI.cpp
+++ network/RESTAPI.cpp
@@ -11,8 +11,7 @@
 // Licensed under the GPLv2 License (https://opensource.org/licenses/GPL-2.0)
 //
 /*
-*   Copyright (C) 2019 by Jonathan Naylor G4KLX
-*   Copyright (C) 2019-2023 by Bryan Biedenkapp N2PLL
+*   Copyright (C) 2023 by Bryan Biedenkapp N2PLL
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
