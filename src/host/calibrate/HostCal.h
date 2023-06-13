10fc1ea015d1240b18f5cbbfe86bf0c19d4f6829
Bryan Biedenkapp <gatekeep@gmail.com>
2023-06-12 22:29:51 -0400

    bump copyright information;

--- src/host/calibrate/HostCal.h
+++ src/host/calibrate/HostCal.h
@@ -13,7 +13,7 @@
 /*
 *   Copyright (C) 2015,2016,2017 by Jonathan Naylor G4KLX
 *   Copyright (C) 2017,2018 by Andy Uribe CA6JAU
-*   Copyright (C) 2017-2021 by Bryan Biedenkapp N2PLL
+*   Copyright (C) 2017-2023 by Bryan Biedenkapp N2PLL
 *
 *   This program is free software; you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
