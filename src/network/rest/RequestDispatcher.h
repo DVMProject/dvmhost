5c995535d8aa2f4d3765b96cb9665ebc71db35be
Justin Richards <ratmandu@gmail.com>
2023-12-11 18:58:59 -0600

    Adding CORS headers to default replies

--- src/network/rest/RequestDispatcher.h
+++ src/network/rest/RequestDispatcher.h
@@ -156,6 +156,12 @@ namespace network
                             }
 
                             //what = matcher.first;
+
+                            // ensure CORS headers are added
+                            reply.headers.add("Access-Control-Allow-Origin", "*");
+                            reply.headers.add("Access-Control-Allow-Methods", "*");
+                            reply.headers.add("Access-Control-Allow-Headers", "*");
+                            
                             matcher.second->handleRequest(request, reply, what);
                             return;
                         }
