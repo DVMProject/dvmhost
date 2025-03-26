9b0371d80e8cdb16b6a40db931070006d0162dc5
Bryan Biedenkapp <gatekeep@gmail.com>
2025-03-26 16:36:29 -0400

    update build bumper for R04G20 to R04G21;

--- src/common/Defines.h
+++ src/common/Defines.h
@@ -108,7 +108,7 @@ typedef unsigned long long  ulong64_t;
 #define __EXE_NAME__ ""
 
 #define VERSION_MAJOR       "04"
-#define VERSION_MINOR       "20"
+#define VERSION_MINOR       "21"
 #define VERSION_REV         "G"
 
 #define __NETVER__ "DVM_R" VERSION_MAJOR VERSION_REV VERSION_MINOR
