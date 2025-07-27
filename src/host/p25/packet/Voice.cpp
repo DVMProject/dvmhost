efe1357ef079d0398421380922d73d2750113eb1
Bryan Biedenkapp <gatekeep@gmail.com>
2025-07-27 15:56:58 -0400

    experimental fix to ignore wildly illegal TGIDs in the HDU;

--- src/host/p25/packet/Voice.cpp
+++ src/host/p25/packet/Voice.cpp
@@ -183,6 +183,11 @@ bool Voice::process(uint8_t* data, uint32_t len)
             m_p25->m_rfLastDstId = lc.getDstId();
             m_p25->m_rfLastSrcId = lc.getSrcId();
 
+            // is the TGID presented in the HDU valid?
+            if (!acl::AccessControl::validateTGId(lc.getDstId(), m_p25->m_forceAllowTG0)) {
+                m_p25->m_rfLastDstId = 0U;
+            }
+
             m_rfLastHDU = lc;
             m_rfLastHDUValid = true;
 
@@ -294,6 +299,12 @@ bool Voice::process(uint8_t* data, uint32_t len)
                 m_p25->m_txQueue.clear();
 
                 resetRF();
+            } else {
+                // is the TGID presented in the HDU valid?
+                if (m_p25->m_rfLastDstId == 0U && !acl::AccessControl::validateTGId(m_rfLastHDU.getDstId(), m_p25->m_forceAllowTG0)) {
+                    // the TGID in the HDU isn't valid -- we'll override it with the one in our LDU1
+                    m_p25->m_rfLastDstId = dstId;
+                }
             }
 
             if (m_p25->m_enableControl) {
