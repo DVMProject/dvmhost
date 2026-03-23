3595819cc859a1b048648108dae1b9db84fa7e19
Bryan Biedenkapp <gatekeep@gmail.com>
2026-03-23 17:05:09 -0400

    bump version number;

--- src/common/Defines.h
+++ src/common/Defines.h
@@ -110,7 +110,7 @@ typedef unsigned long long  ulong64_t;
 #define __EXE_NAME__ ""
 
 #define VERSION_MAJOR       "05"
-#define VERSION_MINOR       "04"
+#define VERSION_MINOR       "06"
 #define VERSION_REV         "A"
 
 #define __NETVER__ "DVM_R" VERSION_MAJOR VERSION_REV VERSION_MINOR
